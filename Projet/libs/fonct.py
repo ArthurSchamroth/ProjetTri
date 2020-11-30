13f8d66b4fb892ef758881b046d719b748876045
Arthur Schamroth <49406566+ArthurSchamroth@users.noreply.github.com>
2020-11-30 20:03:37 +0100

    Correct utilisation console

--- Projet/libs/fonct.py
+++ Projet/libs/fonct.py
@@ -108,7 +108,7 @@ def fichier_en_forme(demand):
 continu = "Oui"
 choix_console = input("Voulez_vous travailler exclusivement en console ? (Oui/Non) ")
 
-if choix_console == "Oui":
+if choix_console == "Oui" or choix_console == "oui":
 
     print("Sélectionner ce que vous voulez faire parmis ces propositions :")
     print("1) 'Récupérer fichiers' afin de récupérer tous les fichiers présents dans votre dossier"
@@ -119,32 +119,36 @@ if choix_console == "Oui":
     print("4) 'Demander type' afin de récupérer tous les fichiers d'un certain type")
     print("5) 'Récupérer type' afin de récupérer tous les types de fichiers")
 
-    while continu == "Oui":
+    while continu == "Oui" or continu == "oui":
         choix = input("Que voulez faire ? ")
 
-        if choix == "Récuperer fichiers":
+        if choix == "Récuperer fichiers" or choix == "récuperer fichiers":
+            print("Voici tous vos fichiers présents dans votre dossier de téléchargements et ses sous-dossiers : ")
             print(recuperer_fichiers())
             continu = input("Voulez-vous continuer à travailler en console ? (Oui/Non) ")
 
-        elif choix == "Grouper fichiers":
+        elif choix == "Grouper fichiers" or choix == "grouper fichiers":
             grouper_fichiers()
             print("Les sous-dossiers viennent de se créer dans votre dossier de téléchargements")
             continu = input("Voulez-vous continuer à travailler en console ? (Oui/Non) ")
 
-        elif choix == "Déplacer fichiers":
+        elif choix == "Déplacer fichiers" or choix == "déplacer fichiers":
             deplacer_fichiers()
             print("Tous vos fichiers viennent de se trier dans leur sous-dossier respectif")
             continu = input("Voulez-vous continuer à travailler en console ? (Oui/Non) ")
 
-        elif choix == "Demander type":
+        elif choix == "Demander type" or choix == "demander type":
             type_ext = input("Quel type de fichier voulez-vous récupérer ? ")
             print("Voici vos fichiers {}".format(type_ext))
             print(demander_type(type_ext))
             continu = input("Voulez-vous continuer à travailler en console ? (Oui/Non) ")
 
-        elif choix == "Récupérer type":
+        elif choix == "Récupérer type" or choix == "récupérer type":
+            print("Voici tous les types de fichier présents dans votre dossier de téléchargements : ")
             print(recuperer_type())
             continu = input("Voulez-vous continuer à travailler en console ? (Oui/Non) ")
+        else:
+            print("Veuillez entrer un choix disponible ")
 
 else:
     print("Alors vous pouvez ouvrir l'interface graphique.")
