822f82fd0e2be4734c60f12f9093c737b4315372
Arthur Schamroth <49406566+ArthurSchamroth@users.noreply.github.com>
2020-12-06 17:56:42 +0100

    Correction database
    nouvelle réponse si on demande comparaison à la date du jour même, modification format de réponse

--- Projet/database.py
+++ Projet/database.py
@@ -6,6 +6,7 @@ import datetime
 cnx = mysql.connector.connect(user="root", password="admin", host="127.0.0.1", database="infos")
 db_name = "infos"
 tables = {}
+dates = []
 mycursor = cnx.cursor()
 dict = recuperer_fichiers()
 
@@ -21,9 +22,8 @@ def creer_Table_Comparaison():
 
 
 def remplir_table_infos():
-    print(dict.items())
     sqlFormula = "INSERT INTO Infos (Date, Nom, Type) VALUES (%s, %s, %s)"
-    for cle, value in dict.items():
+    for cle, value in dicti.items():
         for i in value:
             information = (datetime.datetime.today().strftime('%Y-%m-%d'), i, cle)
             mycursor.execute(sqlFormula, information)
@@ -45,14 +45,24 @@ def compter():
     return total
 
 
+def recuperer_date():
+    mycursor.execute("select distinct Date from comparaison")
+    for i in mycursor.fetchall():
+        dates.append(i)
+    return dates
+
+
 def comparaison(date):
-    # Voir si pas possible de mettre deux dates, une de début et une de fin de comparaison
     mycursor.execute('SELECT Max(Total) FROM comparaison WHERE Date LIKE \'%' + date + '%\'')
     return mycursor.fetchone()
 
 
 def result_comparaison(date):
-    mycursor.execute('SELECT Max(Total) FROM comparaison WHERE Date LIKE Date(Now())')
-    total_today = mycursor.fetchone()[0]
-    difference = total_today - comparaison(date)[0]
-    return "Depuis le {}, vous avez téléchargé {} nouveaux fichier(s)".format(date, difference)
+    if date == datetime.datetime.today().strftime("%Y-%m-%d"):
+        return "A ce jour, vous possedez {} fichiers dans votre dossier de téléchargements".format(compter())
+    else:
+        mycursor.execute('SELECT Max(Total) FROM comparaison WHERE Date LIKE Date(Now())')
+        total_today = mycursor.fetchone()[0]
+        difference = total_today - comparaison(date)[0]
+        return "Depuis le {}, vous avez téléchargé {} nouveaux fichier(s).\n Ainsi vous êtes à {} fichiers dans votre "\
+               "dossier de téléchargements".format(date, difference, compter())
