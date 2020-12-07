9bda4d36cf82ae60188a199a05dcb925640f9440
Arthur Schamroth <schamrotharthur@gmail.com>
2020-12-07 20:13:35 +0100

    Mise à jour

--- Projet/main.py
+++
@@ -1,9 +0,0 @@
-from libs.fonct import fonct_console
-
-choix_console = input("Voulez_vous travailler exclusivement en console ? (Oui/Non) ")
-
-if choix_console == "Oui" or choix_console == "oui":
-    fonct_console()
-else:
-    print("coucou")
-    # Quand on tape 'Non' ca doit lancer l'interface graphique
