c7b25d1a0bcd02ec0d705ba092e77af5ddf3762d
Arthur Schamroth <schamrotharthur@gmail.com>
2020-12-07 20:29:49 +0100

    retirer le test

--- main.py
+++ main.py
@@ -8,5 +8,4 @@ if choix_console == "Oui" or choix_console == "oui":
     fonct_console()
 else:
     commencer()
-    print("Bonjour")
     # Quand on tape 'Non' ca doit lancer l'interface graphique
