1a0f51793d3b2737e7baae9bc05f042bbb6a0491
Arthur Schamroth <schamrotharthur@gmail.com>
2020-12-08 14:41:59 +0100

    commentaire main

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from libs.interf_graph import commencer
 choix_console = input("Voulez_vous travailler exclusivement en console ? (Oui/Non) ")
 
 if choix_console == "Oui" or choix_console == "oui":
+    # Programme en console
     fonct_console()
 else:
+    # Programme avec interface graphique
     commencer()
-    # Quand on tape 'Non' ca doit lancer l'interface graphique
