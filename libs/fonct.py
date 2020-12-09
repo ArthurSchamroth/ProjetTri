0f1712edcff4c2507dc7ab34a001c369be2e09d6
Firmin <firminmoyen@gmail.com>
2020-12-09 17:00:42 +0100

    ergonomie mode console et dossier autre

--- libs/fonct.py
+++ libs/fonct.py
@@ -22,10 +22,16 @@ def recuperer_fichiers():
                 element = os.path.splitext(j)
                 # new type
                 if element[1][1:] not in dicti.keys():
-                    dicti[element[1][1:]] = [element[0]]
+                    if element[1] == "":
+                        dicti["dossier"] = [element[0]]
+                    else:
+                        dicti[element[1][1:]] = [element[0]]
                 # not new type
                 else:
-                    dicti[element[1][1:]].append(element[0])
+                    if element[1] == "":
+                        dicti["dossier"].append(element[0])
+                    else:
+                        dicti[element[1][1:]].append(element[0])
         # not repertory
         else:
             element = os.path.splitext(i)
@@ -130,40 +136,27 @@ def ouverture_appli(fichier):
 
 def fonct_console():
     continu = "Oui"
-    print("Sélectionner ce que vous voulez faire parmis ces propositions :")
+    print("Sélectionner un numéro de propositions :")
     print("1) 'Récupérer fichiers' afin de récupérer tous les fichiers présents dans votre dossier"
           " de téléchargements")
-    print("2) 'Grouper fichiers' afin de créer tous les sous-dossiers de votre dossier de téléchargements")
-    print("3) 'Déplacer fichiers' afin de déplacer tous les fichiers de votre dossier de téléchargement dans son "
-          "sous-dossier")
-    print("4) 'Demander type' afin de récupérer tous les fichiers d'un certain type")
-    print("5) 'Récupérer type' afin de récupérer tous les types de fichiers")
+    print("2) 'Demander type' afin de récupérer tous les fichiers d'un certain type")
+    print("3) 'Récupérer type' afin de récupérer tous les types de fichiers")
 
     while continu == "Oui" or continu == "oui":
         choix = input("Que voulez faire ? ")
 
-        if choix == "Récupérer fichiers" or choix == "récupérer fichiers":
+        if choix == "1":
             print("Voici tous vos fichiers présents dans votre dossier de téléchargements et ses sous-dossiers : ")
             print(recuperer_fichiers())
             continu = input("Voulez-vous continuer à travailler en console ? (Oui/Non) ")
 
-        elif choix == "Grouper fichiers" or choix == "grouper fichiers":
-            grouper_fichiers()
-            print("Les sous-dossiers viennent de se créer dans votre dossier de téléchargements")
-            continu = input("Voulez-vous continuer à travailler en console ? (Oui/Non) ")
-
-        elif choix == "Déplacer fichiers" or choix == "déplacer fichiers":
-            deplacer_fichiers()
-            print("Tous vos fichiers viennent de se trier dans leur sous-dossier respectif")
-            continu = input("Voulez-vous continuer à travailler en console ? (Oui/Non) ")
-
-        elif choix == "Demander type" or choix == "demander type":
+        elif choix == "2":
             type_ext = input("Quel type de fichier voulez-vous récupérer ? ")
             print(ajouter_description(type_ext))
             print(demander_type(type_ext))
             continu = input("Voulez-vous continuer à travailler en console ? (Oui/Non) ")
 
-        elif choix == "Récupérer type" or choix == "récupérer type":
+        elif choix == "3":
             print("Voici tous les types de fichier présents dans votre dossier de téléchargements : ")
             print(recuperer_type())
             continu = input("Voulez-vous continuer à travailler en console ? (Oui/Non) ")
@@ -176,10 +169,5 @@ def fonct_console():
 
 
 # librairie shh1 et md5 (moins bien (sécurisée))
-"""a = "bonjour"
-b = "bonjour"
 
-result = hashlib.md5(a.encode("utf-8"))
-print(result.hexdigest())
-result2 = hashlib.md5(b.encode("utf-8"))
-print(result2.hexdigest())"""
+
